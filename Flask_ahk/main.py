534c139c007349f31aa0e2f6b60a060e605b5b2c
surya-u <90176259+surya-u@users.noreply.github.com>
2021-10-15 03:28:30 +0530

    Update main.py

--- Flask_ahk/main.py
+++ Flask_ahk/main.py
@@ -1,24 +1,24 @@
 from flask import Flask, render_template
-app = Flask(__name__)
 from ahk import AHK
 from ahk.window import Window
+
 def getActiveList():
   ahk = AHK()
-  win = list(ahk.windows())
   active = []
-  for w in win:
+  for w in ahk.windows():
       a=w.title
       if a:
         a=a.decode('utf-8')
         active.append(a)
   return active
 
-print("calling getactivelist function")
-print(getActiveList())
-
-@app.route("/")
-def return_file():
-  return render_template('index.html',len=len(getActiveList()),active=getActiveList())
-
 if __name__ == "__main__":
-  app.run(debug=True)
+  app = Flask(__name__)
+
+  @app.route("/")
+  def return_file():
+    print("calling getactivelist function")
+    ar = getActiveList()
+    return render_template('index.html',len=len(ar),active=ar)
+
+  app.run()
